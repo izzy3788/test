70a42873b99b33496178432fd4081e65d11785b2
izzy <endhee47@gmail.com>
2026-01-19 09:58:08 +0900

    유저 리스트 화면 완료

---
+++ my-app/app/user-list/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import Image from "next/image";
+
+const users = [
+  { id: 1, name: "홍길동", team: "생산 1팀", image: "/image/user1.jpg" },
+  { id: 2, name: "홍길동", team: "생산 2팀", image: "/image/user2.jpg" },
+  { id: 3, name: "홍길동", team: "생산 3팀", image: "/image/user3.jpg" },
+  { id: 4, name: "홍길동", team: "생산 4팀", image: "/image/user4.jpg" },
+  { id: 5, name: "홍길동", team: "생산 4팀", image: "/image/user4.jpg" },
+];
+
+export default function UserTypeSelect() {
+  return (
+    <div className="min-h-screen bg-[#EDEEF1]">
+      {/* 상단 영역 */}
+      <div className="flex flex-col items-center gap-6">
+        <h1 className="text-[#2B2C36] text-[24px] font-bold text-center mt-18 -mb-2">
+          사용자 이름을 검색해 주세요
+        </h1>
+
+        <Input type="text" placeholder="사용자 이름 검색" className="w-90" />
+      </div>
+
+      {/* 유저 리스트 */}
+      <div className="px-10 mt-16 pb-24 max-w-300 mx-auto">
+        <div className="grid grid-cols-4 gap-6">
+          {users.map((user) => (
+            <Card key={user.id} className="rounded-xl">
+              <CardContent className="flex flex-col items-center pt-2">
+                <Avatar className="h-30 w-30">
+                  <AvatarImage src={user.image} alt={user.name} />
+                  <AvatarFallback className="relative">
+                    <Image
+                      src="/image/profile.svg"
+                      alt="default profile"
+                      fill
+                      className="object-cover rounded-full"
+                    />
+                  </AvatarFallback>
+                </Avatar>
+
+                <p className="mt-5 text-[18px] font-bold text-[#2B2C36]">
+                  {user.name}
+                </p>
+                <p className="pt-1 text-sm text-[#6C7A8B]">{user.team}</p>
+              </CardContent>
+
+              <CardFooter>
+                <Button
+                  variant="secondary"
+                  className="w-full bg-[#F9F5FF] text-[#6F3BF2] hover:bg-[#EDE0FF]"
+                >
+                  선택하기
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+
+      {/* 하단 고정 버튼 */}
+      <Button
+        size="lg"
+        className="
+          fixed
+          bottom-0
+          left-0
+          w-full
+          rounded-none
+          bg-[#474F5D]
+          text-white
+          hover:bg-[#3C434E]
+          z-50
+        "
+      >
+        나가기
+      </Button>
+    </div>
+  );
+}
